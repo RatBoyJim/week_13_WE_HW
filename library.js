627120a37e8a6648ae868d08415940538a8e5a25
RatBoyJim <ratboyjim@gmail.com>
2021-01-23 10:05:29 +0000

    final commit

---
+++ library.js
@@ -0,0 +1,25 @@
+class Library {
+  constructor(books = []) {
+    this.books = books;
+  }
+  bookCount(){
+    return this.books.length;
+  }
+
+  addBook(newBook){
+    this.books.push(newBook);
+  }
+
+  addBooks(newBooks){
+    newBooks.forEach(book => this.books.push(book));
+  }
+
+  printInventory(){
+    const {author, title} = book;
+    this.books.forEach((book) => {
+      console.log(`${title} by ${author}`);
+    });
+  }
+};
+
+export default Library;
